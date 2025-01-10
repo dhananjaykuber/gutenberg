ea694c1376bfd6f91ad58029755b188eba059d5b
Dhananjay Kuber <dhananjay.kuber@rtcamp.com>
2025-01-10 11:58:27 +0530

    fix: make label short and remove alt text from editor

--- packages/block-library/src/post-featured-image/edit.js
+++ packages/block-library/src/post-featured-image/edit.js
@@ -247,7 +247,10 @@ export default function PostFeaturedImageEdit( {
 									: __( 'Link to post' )
 							}
 							onChange={ () =>
-								setAttributes( { isLink: ! isLink } )
+								setAttributes( {
+									isLink: ! isLink,
+									useEmptyAlt: isLink ? useEmptyAlt : false,
+								} )
 							}
 							checked={ isLink }
 						/>
@@ -310,9 +313,7 @@ export default function PostFeaturedImageEdit( {
 						>
 							<ToggleControl
 								__nextHasNoMarginBottom
-								label={ __(
-									'Use empty alt text (decorative image)'
-								) }
+								label={ __( 'Use empty alt text' ) }
 								help={ __(
 									'Enable this if the image is purely decorative and should be ignored by screen readers.'
 								) }
@@ -375,6 +376,27 @@ export default function PostFeaturedImageEdit( {
 	};
 
 	/**
+	 * Get the alt text for the image.
+	 *
+	 * @return {string} The alt text for the image.
+	 */
+	const getAltText = () => {
+		if ( useEmptyAlt ) {
+			return '';
+		}
+
+		if ( media?.alt_text ) {
+			return sprintf(
+				// translators: %s: The image's alt text.
+				__( 'Featured image: %s' ),
+				media.alt_text
+			);
+		}
+
+		return __( 'Featured image' );
+	};
+
+	/**
 	 * When the post featured image block is placed in a context where:
 	 * - It has a postId (for example in a single post)
 	 * - It is not inside a query loop
@@ -416,15 +438,7 @@ export default function PostFeaturedImageEdit( {
 					<img
 						className={ borderProps.className }
 						src={ temporaryURL || mediaUrl }
-						alt={
-							media && media?.alt_text
-								? sprintf(
-										// translators: %s: The image's alt text.
-										__( 'Featured image: %s' ),
-										media.alt_text
-								  )
-								: __( 'Featured image' )
-						}
+						alt={ getAltText() }
 						style={ imageStyles }
 					/>
 					{ temporaryURL && <Spinner /> }
